2e92c4f1cd13384618802596c59dabb97fd384d9
Vijay0520 <vijaykumargoudagouda@gamil.com>
2025-09-23 20:37:23 +0530

    final push

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express=require('express')
+// const {users}=require('./data/users.json')
+
+//importing the routers
+const usersRouter=require('./routes/users');
+const booksRouter=require('./routes/books');
 
 const app=express()
 
@@ -6,11 +11,16 @@ const PORT=8081
 
 app.use(express.json())
 
+
 app.get('/',(req,res)=>{
     res.status(200).json({
         message:"Home Page"
     })
 })
+
+app.use('/users',usersRouter);
+app.use('/books',booksRouter);
+
 
 app.listen(PORT,()=>{
     console.log(`server is up and running in http://localhost:${8081}`);
